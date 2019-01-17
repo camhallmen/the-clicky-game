5de158444acd155f82a49ab09641c1e4764fbad0
thallmen <camhallmen@gmail.com>
2019-01-16 19:01:40 -0500

    re-trying deployment

---
+++ the-clicky-game/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./components/Header/index"
+import Body from "./components/Body/index"
+
+function App () {
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  )
+}
+
+export default App;
