7fb6c262319b083306a8c401197db5dc9be3148d
thallmen <camhallmen@gmail.com>
2019-01-17 21:28:58 -0500

    Updates

---
+++ precache-manifest.881ea4fe8dbaa9ab822474e92cd8e34f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4edc6bd39a6bace4ccbc66b0e9c0bc83",
+    "url": "/the-clicky-game/static/media/pikachu.4edc6bd3.png"
+  },
+  {
+    "revision": "5e70e261fbebc19a649c869f476596be",
+    "url": "/the-clicky-game/static/media/grass-background.5e70e261.svg"
+  },
+  {
+    "revision": "8501879536f932a8a141",
+    "url": "/the-clicky-game/static/js/runtime~main.85018795.js"
+  },
+  {
+    "revision": "da3eb68a13b5c93cdd3a",
+    "url": "/the-clicky-game/static/js/main.da3eb68a.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/the-clicky-game/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "da3eb68a13b5c93cdd3a",
+    "url": "/the-clicky-game/static/css/main.8b1fc4fb.chunk.css"
+  },
+  {
+    "revision": "338a72a87c0c402ee8c0b3783e8eea25",
+    "url": "/the-clicky-game/index.html"
+  }
+];
